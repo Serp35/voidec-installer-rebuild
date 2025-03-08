f0b994408205580fdc1cbef80dc276d50fd80fc5
Serp35 <maths9533@gmail.com>
2025-03-08 20:14:54 +0000

    Update turn.sh

--- turn.sh
+++ turn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -O https://maths-rpi.ddns.net/voidec/scripts/rebuild.sh
+curl -O https://maths-rpi.ddns.net/voidec/downloads/scripts/rebuild.sh
 mv rebuild.sh /usr/local/bin/voidec-rebuild
 voidec-rebuild generate
